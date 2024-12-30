42eff692faa2e19009ca29ada5d9cfe99dcfc72c
Amir Muratov <amirmuratov@MacBook-Pro-Amir.local>
2024-12-30 23:17:12 +0100

    remove exp for testing

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import {FloatingNav} from "@/components/ui/FloatingNav";
 import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import {navItems} from "@/data";
-import Experience from "@/components/Experience";
+// import Experience from "@/components/Experience";
 
 export default function Home() {
     return (
